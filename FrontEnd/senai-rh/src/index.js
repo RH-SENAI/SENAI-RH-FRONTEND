0b6529747235f0beaeed3294527099dd3982776c
Gustavo Keiji <gugu.keiji.2004@gmail.com>
2022-03-24 23:15:24 -0300

    feat: Tela Cadastro Atividades iniciada

--- FrontEnd/senai-rh/src/index.js
+++ FrontEnd/senai-rh/src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  Route,
+  BrowserRouter as Router,
+  Switch
+} from 'react-router-dom';
 import './index.css';
-import App from './Pages/Home/App';
+import CadastrarAtividades from './Pages/CadastrarAtividades/cadastrarAtividades.jsx';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const routing = (
+  <Router>
+    <div>
+      <Switch>
+        <Route exact path="/" component={CadastrarAtividades}/>
+
+      </Switch>
+    </div>
+  </Router>
+)
+
+ReactDOM.render(routing, document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
