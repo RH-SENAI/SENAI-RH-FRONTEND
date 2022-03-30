cee510bb8e8eb530f18257446df2392b2f7d7408
Edson Junior <ejr_04@outlook.com>
2022-03-30 20:55:48 -0300

    feat: modal na tela de cursos

--- FrontEnd/senai-rh/src/Pages/Cursos/cursosRapidos.jsx
+++ FrontEnd/senai-rh/src/Pages/Cursos/cursosRapidos.jsx
@@ -6,8 +6,42 @@ import logica from '../../Assets/img/logica.png'
 import coracao from '../../Assets/img/coracao.svg'
 import relogio from '../../Assets/img/relogio.svg'
 import local from '../../Assets/img/local.svg'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Modal from 'react-modal';
 
 export default function CursosRapidos() {
+    const [modalIsOpen, setIsOpen] = React.useState(false);
+    let subtitle;
+
+
+    function openModal() {
+        setIsOpen(true);
+    }
+
+    function afterOpenModal() {
+        subtitle.style.color = '#f00';
+        
+    }
+
+    function closeModal() {
+        setIsOpen(false);
+    }
+
+    const custonModal = {
+        content: {
+            left: '5%',
+            top: '10%',
+            background: 'f2f2f2',
+            bottom: 'auto',
+            borderRadius: '20px',
+            width: '90%',
+            height: '80%',
+            
+        }
+
+    }
+
     return (
         <div>
             <HeaderFuncionario />
@@ -21,33 +55,11 @@ export default function CursosRapidos() {
 
             <h1>Cursos</h1>
             <section className='container_cursos '>
-                
+
                 <div className='wrap'>
                     <div className='container_wrap'>
 
-                    
-                    <section className='box_cursos'>
-                        <img className='banner' src={logica} alt="" />
-                        <h2>Logica de Programação</h2>
-                        <p> <img src={relogio} alt="duracao" /> 20 Horas </p>
-                        <p><img src={local} alt="local" /> EAD</p>
-                        <div className='circulo_coracao'>
-                            <img className='coracao' src={coracao} alt="" />
-                        </div>
-                        {/* <img src={} alt="" /> */}
-                    </section>
 
-                        <section className='box_cursos'>
-                            <img className='banner' src={logica} alt="" />
-                            <h2>Logica de Programação</h2>
-                            <p> <img src={relogio} alt="duracao" /> 20 Horas </p>
-                            <p> <img src={local} alt="local" /> EAD</p>
-                            <div className='circulo_coracao'>
-                                <img className='coracao' src={coracao} alt="" />
-                            </div>
-                            {/* <img src={} alt="" /> */}
-                        </section>
-                        
                         <section className='box_cursos'>
                             <img className='banner' src={logica} alt="" />
                             <h2>Logica de Programação</h2>
@@ -56,10 +68,63 @@ export default function CursosRapidos() {
                             <div className='circulo_coracao'>
                                 <img className='coracao' src={coracao} alt="" />
                             </div>
-                            {/* <img src={} alt="" /> */}
+                            <button onClick={openModal}>Modal</button>
+
+                            <Modal
+                                style={custonModal}
+                                isOpen={modalIsOpen}
+                                onAfterOpen={afterOpenModal}
+                                onRequestClose={closeModal}
+                            >
+                                <button onClick={closeModal}>Close</button>
+                                <img src={logica} alt="" />
+                                <h3>Opa</h3>
+                            </Modal>
+                        </section>
+                        <section className='box_cursos'>
+                            <img className='banner' src={logica} alt="" />
+                            <h2>Logica de Programação</h2>
+                            <p> <img src={relogio} alt="duracao" /> 20 Horas </p>
+                            <p><img src={local} alt="local" /> EAD</p>
+                            <div className='circulo_coracao'>
+                                <img className='coracao' src={coracao} alt="" />
+                            </div>
+                            <button onClick={openModal}>Modal</button>
+
+                            <Modal
+                                style={custonModal}
+                                isOpen={modalIsOpen}
+                                onAfterOpen={afterOpenModal}
+                                onRequestClose={closeModal}
+                            >
+                                <button onClick={closeModal}>Close</button>
+                                <img src={logica} alt="" />
+                                <h3>Opa</h3>
+                            </Modal>
+                        </section>
+                        <section className='box_cursos'>
+                            <img className='banner' src={logica} alt="" />
+                            <h2>Logica de Programação</h2>
+                            <p> <img src={relogio} alt="duracao" /> 20 Horas </p>
+                            <p><img src={local} alt="local" /> EAD</p>
+                            <div className='circulo_coracao'>
+                                <img className='coracao' src={coracao} alt="" />
+                            </div>
+                            <button onClick={openModal}>Modal</button>
+
+                            <Modal
+                                style={custonModal}
+                                isOpen={modalIsOpen}
+                                onRequestClose={closeModal}
+                            >
+                                <button onClick={closeModal}>Close</button>
+                                <img src={logica} alt="" />
+                                <h3>Opa</h3>
+                            </Modal>
                         </section>
 
-                </div>
+
+                    </div>
                 </div>
 
                 <div className='container_img'>
