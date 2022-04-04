052a96f521567804bda46797c964778835f466c3
Edson Júnior <edson.vale@portalsesisp.org.br>
2022-04-04 17:18:01 -0300

    Fix: ajuste na tela cadastro de curso

--- FrontEnd/senai-rh/src/Pages/Cursos/cadastrarCursos.jsx
+++ FrontEnd/senai-rh/src/Pages/Cursos/cadastrarCursos.jsx
@@ -12,27 +12,71 @@ export default function CadastrarCursos() {
                 <h1>Cadastro de Cursos</h1>
             </div>
             <div className="container_forms_cursos">
-            <div className="box_img_cadastroCurso">
-                <img src={cadastroCurso} alt="" />
-            </div>
+                <div className="box_img_cadastroCurso">
+                    <img src={cadastroCurso} alt="" />
+                </div>
                 <form className="box_forms_cursos" action="">
-                    <div  className="inputCadastro_cursos">
+                    <div className="inputCadastro_cursos">
 
-                    <label>Cursos</label>
-                    <input type="text" placeholder="Digite o nome do curso" className="inputCadastro_cursos" />
-                    <label>Data de finalizar</label>
-                    <input type="date" />
-                    <label>Descrição</label>
-                    <input type="text"  />
-                    <label>Imagem</label>
-                    <input type="file"  />
-                    <label>Modelo</label>
-                    <input type="radio" name="Presencial"  /> <br />
-                    <label></label>
-                    <input type="radio" name="EAD" /> <br />
-                    <label>Site do Curso</label>
-                    <input type="text"  />
+                        <div className="dis">
+                            <div className="flex_co">
+                                <label>Cursos</label>
+                                <input type="text" placeholder="Digite o nome do curso" />
+                            </div>
+
+                            <div className="flex_co">
+                                <label>Data de finalizar</label>
+                                <input type="date" />
+                            </div>
+                        </div>
+
+                        <div className="flex_co">
+                            <label>Descrição</label>
+                            <input type="text" placeholder="Digite aqui a detalhes do seu curso"/>
+                        </div>
+
+                        <div className="dis">
+                            <div className="flex_co">
+                                <label>Imagem</label>
+                                <label className="label_arquivo" htmlFor="arquivo">Imagem do curso</label>
+                                <input id="arquivo" className="input_file" type="file"  />
+                            </div>
+
+                            {/* <div className="flex_co ">
+                                    <label htmlFor="Presencial">Modelo</label>
+                                <div>
+                                    <input id="Presencial" type="radio" name="web" />Presencial <br />
+                                </div>
+                                <div className="radio">
+                                    <label></label>
+                                    <input type="radio" name="web" /> EAD <br />
+
+                                </div>
+                            </div> */}
+
+                            <div className="container_radio">
+                                <h2>Modelo</h2>
+                                <label htmlFor="">
+                                    <input type="radio" name="r1" id="radio1"
+                                    className="ativo" checked />
+                                    Presencial
+                                    <span className="check"></span>
+                                </label>
+                                <label htmlFor="">
+                                    <input type="radio" name="r1" id="radio2"
+                                    className="ativo" checked />
+                                    EAD
+                                    <span className="check"></span>
+                                </label>
+                            </div>
+
+                        </div>
+                        <div className="flex_co">
+                            <label>Site do Curso</label>
+                            <input type="text" placeholder="Coloque aqui o link para inscrever-se no seu curso " />
+                        </div>
                     </div>
+                         <button className="botaoCadastro">Inscreva-se</button>
                 </form>
             </div>
         </div>
