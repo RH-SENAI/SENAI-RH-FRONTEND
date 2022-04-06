6b3a7b8c72df603bd7918ac4e6d9251128e04e26
Edson Junior <ejr_04@outlook.com>
2022-04-05 21:49:29 -0300

    fix: listagem de cursos

--- FrontEnd/senai-rh/src/Pages/Cursos/cadastrarCursos.jsx
+++ FrontEnd/senai-rh/src/Pages/Cursos/cadastrarCursos.jsx
@@ -2,7 +2,47 @@ import HeaderAdm from "../../components/Header/headerAdm";
 import cadastroCurso from '../../Assets/img/cadastroCurso.svg'
 import '../../Assets/Css/cadastroCursos.css'
 
+import api from '../../Services/api'
+import { useState } from "react";
+
 export default function CadastrarCursos() {
+    const [idEmpresa, setIdEmpresa] = useState(0)
+    const [nomeCurso, setNomeCurso] = useState('')
+    const [descricaoCurso, setDescricaoCurso] = useState('')
+    const [siteCurso, setSiteCurso] = useState('')
+    const [modalidadeCurso, setModalidadeCurso] = useState([])
+    const [dataFinalização, setDataFinalizacao] = useState(new Date())
+    const [caminhoImagemCurso, setCaminhoImagemCurso] = useState('')
+    const [isLoading, setisLoading] = useState(false)
+    const [erroMensagem, setErroMensagem] = useState('');
+
+
+
+    function efetuarCadastro(event) {
+        event.preventDefault();
+        api.post('/Usuarios', {
+            idEmpresa: idEmpresa,
+            nomeCurso: nomeCurso,
+            descricaoCurso: descricaoCurso,
+            siteCurso: siteCurso,
+            modalidadeCurso: modalidadeCurso,
+            dataFinalização: dataFinalização,
+            imagem: caminhoImagemCurso
+
+        })
+            .then(resposta => {
+                if (resposta.status === 200) {
+                    console.log('Cadastro Realizado')
+                }
+            })
+            .catch(() => {
+                setErroMensagem('Cadastro não realizado!')
+
+                setisLoading(false)
+
+            });
+    }
+
     return (
         <div>
             <HeaderAdm />
@@ -15,31 +55,31 @@ export default function CadastrarCursos() {
                 <div className="box_img_cadastroCurso">
                     <img src={cadastroCurso} alt="" />
                 </div>
-                <form className="box_forms_cursos" action="">
+                <form onSubmit={efetuarCadastro} className="box_forms_cursos" action="">
                     <div className="inputCadastro_cursos">
 
                         <div className="dis">
                             <div className="flex_co">
                                 <label>Cursos</label>
-                                <input type="text" placeholder="Digite o nome do curso" />
+                                <input  onChange={(campo) => setNomeCurso(campo.target.value)} value={nomeCurso} type="text" name="nomeCurso" placeholder="Digite o nome do curso" />
                             </div>
 
                             <div className="flex_co">
                                 <label>Data de finalizar</label>
-                                <input type="date" />
+                                <input  onChange={(campo) => setDataFinalizacao(campo.target.value)} name="data" value={dataFinalização} type="date" />
                             </div>
                         </div>
 
-                        <div className="flex_co">
+                        <div className="flex_co largura">
                             <label>Descrição</label>
-                            <input type="text" placeholder="Digite aqui a detalhes do seu curso"/>
+                            <input  onChange={(campo) => setDescricaoCurso(campo.target.value)} value={descricaoCurso} name="descricaoCurso" type="text" placeholder="Digite aqui a detalhes do seu curso" />
                         </div>
 
                         <div className="dis">
                             <div className="flex_co">
                                 <label>Imagem</label>
                                 <label className="label_arquivo" htmlFor="arquivo">Imagem do curso</label>
-                                <input id="arquivo" className="input_file" type="file"  />
+                                <input  onChange={(campo) => setCaminhoImagemCurso(campo.target.value)} value={caminhoImagemCurso} id="arquivo" name="caminhoImagemCurso" className="input_file" type="file" />
                             </div>
 
                             {/* <div className="flex_co ">
@@ -58,25 +98,27 @@ export default function CadastrarCursos() {
                                 <h2>Modelo</h2>
                                 <label htmlFor="">
                                     <input type="radio" name="r1" id="radio1"
-                                    className="ativo" checked />
+                                        className="ativo" checked />
                                     Presencial
                                     <span className="check"></span>
                                 </label>
                                 <label htmlFor="">
                                     <input type="radio" name="r1" id="radio2"
-                                    className="ativo" checked />
+                                        className="ativo" checked />
                                     EAD
                                     <span className="check"></span>
                                 </label>
                             </div>
 
                         </div>
-                        <div className="flex_co">
+                        <div className="flex_co largura">
                             <label>Site do Curso</label>
-                            <input type="text" placeholder="Coloque aqui o link para inscrever-se no seu curso " />
+                            <input value={siteCurso} name="siteCurso" onChange={(campo) => setSiteCurso(campo.target.value)} type="text" placeholder="Coloque aqui o link para inscrever-se no seu curso " />
                         </div>
                     </div>
-                         <button className="botaoCadastro">Inscreva-se</button>
+                    <div className="flex_co btn">
+                        <button type="submit" className="botaoCadastro">Inscreva-se</button>
+                    </div>
                 </form>
             </div>
         </div>
--- FrontEnd/senai-rh/src/Pages/Cursos/cursosRapidos.jsx
+++ FrontEnd/senai-rh/src/Pages/Cursos/cursosRapidos.jsx
@@ -89,10 +89,10 @@ export default function CursosRapidos() {
                             listaCursos.map((curso) => {
                                 return (
                                     <section id='imagem' className='box_cursos'>
-                                        {<img onClick={openModal} className='banner' src={curso.caminhoImagemCurso} alt="imagem do curso" />}
+                                        {<img onClick={openModal} className='banner' src={ 'https://raw.githubusercontent.com/RH-SENAI/SENAI-RH-BACKEND/back-gp-2-enzzo/GP2/API_G2/SenaiRH_G2/SenaiRH_G2/StaticFiles/Images/' + curso.caminhoImagemCurso} alt="imagem do curso" />}
                                         {<h2 onClick={openModal}> {curso.nomeCurso} </h2>}
                                         {<p><img className='box_dados_curso' onClick={openModal} src={relogio} alt="duracao" /> {curso.cargaHoraria} Horas </p>}
-                                        {<p><img className='box_dados_curso' onClick={openModal} src={local} alt="duracao" /> {curso.idLogradouroNavigation}  </p>}
+                                        {<p><img className='box_dados_curso' onClick={openModal} src={local} alt="duracao" /> {curso.idEmpresaNavigation.idLocalizacaoNavigation.idLogradouroNavigation.nomeLogradouro}  </p>}
                                         {<div className='circulo_coracao'>
                                             <img className='coracao' src={coracao} alt="" />
                                         </div>}
@@ -107,8 +107,6 @@ export default function CursosRapidos() {
                             <div className='circulo_coracao'>
                                 <img className='coracao' src={coracao} alt="" />
                             </div> */}
-
-
                         <Modal
                             isOpen={modalIsOpen}
                             onRequestClose={closeModal}
