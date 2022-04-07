bc29de02c508469bf95445b4bd5107305a0402b3
MurilloAssis <murillo98asiss@gmail.com>
2022-04-07 17:13:42 -0300

    Minhas atividades....

---
+++ FrontEnd/senaiRHmobile/src/services/auth.js
@@ -0,0 +1,9 @@
+// import AsyncStorage from "@react-native-async-storage/async-storage";
+
+// export const usuarioAutenticado = () => AsyncStorage.getItem('userToken') !== null;
+
+// export const parseJwt = () => {
+//     let base64 = AsyncStorage.getItem('userToken').split('.')[1];
+
+//     return JSON.parse(window.atob(base64))
+// }
