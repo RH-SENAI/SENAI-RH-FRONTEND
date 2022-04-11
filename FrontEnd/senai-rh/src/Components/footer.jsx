f293a52dfa1792abc049ddd20f3d285fd8dd27c4
Samuel <samuel.figs@gmail.com>
2022-04-11 08:43:10 -0300

    modal_ok

---
+++ FrontEnd/senai-rh/src/Components/footer.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react"
+import '../assets/css/footer.css'
+
+export default class Footer extends Component {
+    render() {
+        return (
+            <footer>
+                
+                    <p className="pFooter">
+                        Todos os direitos reservados.
+                    </p>
+                
+            </footer>
+        )
+    }
+}
