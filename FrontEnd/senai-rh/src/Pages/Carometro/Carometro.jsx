47d1f056c0f8d4e21c438fb17654be1ef175b0ef
Matheus Leal Palmuti <47026404881@info.scs>
2022-05-02 16:59:17 -0300

    feat: estiliza Carometro

--- FrontEnd/senai-rh/src/Pages/Carometro/Carometro.jsx
+++ FrontEnd/senai-rh/src/Pages/Carometro/Carometro.jsx
@@ -9,7 +9,8 @@ import Footer from '../../components/footer';
 import PerfilCarometro from '../../assets/img/PerfilCarometro.png'
 import setaSelectLight from '../../assets/img/SetaSelectLight.png'
 import IconLogout from '../../assets/img/IconLogout.png'
-import SetaCarometro from '../../assets/img/SetaCarometro.png'
+import topCarometro from '../../assets/img/topCarometro.png'
+
 //import Modal from 'react-modal';
 //import { ModalAcompanhar } from '../../components/modal/modalAcompanhar';
 
@@ -99,100 +100,47 @@ export default function Carometro() {
     return (
         <body>
             {/* <ModalAcompanhar usuario={listaFuncionarios.find(usuario => usuario.idUsuario == idFuncionarioModal)} showModal={showModal} setShowModal={setShowModal} />  */}
-            <HeaderFuncionario />
-            <main>
+            <div className='g3_whiteBackgroundCarometro'>
+                <HeaderFuncionario />
+                <main>
 
-                <div className="container">
-                    <div className="g3_containerCarometro">
-                        <div className="g3_sidebarCarometro">
-                            <div className='g3_organizacaoSidebar'>
-                                <div className="g3_headerSidebar">
-                                    <img src={PerfilCarometro} alt="fotoPerfilCarometro" />
-                                    <div className="g3_spansSidebar">
-                                        <span className="g3_spanSidebar">Username</span>
-                                        <span className="g3_spanSidebar">Cargo:</span>
-                                    </div>
-
-                                </div>
-                                <div className="g3_bodySidebar">
-                                    <div class='g3_selectCarometro'>
-
-                                        <span className='g3_SpanSidebar'>Equipes <img className='setaSelect' src={setaSelectLight} /></span>
-
-
-                                        <input type='hidden' />
-
-                                        <div class='hiddenCarometro'>
-
-                                            <Link onClick={ToggleMode} className={active ? "textLinkCarometro" : "text_linkCarometro"} to='#' >  Desenvolvedores</Link>
-                                            <Link onClick={ToggleMode} className={active ? "textLinkCarometro" : "text_linkCarometro"} to='#' >  Administradores</Link>
-                                            <Link onClick={ToggleMode} className={active ? "textLinkCarometro" : "text_linkCarometro"} to='#' >  Gestão</Link>
-
-                                        </div>
-
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="g3_footerSidebar">
-                                <span className="g3_spanSidebar">Logout</span>
-                                <img className='g3_iconLogout' src={IconLogout} alt="iconLogout" />
-                            </div>
-
-                        </div>
+                    <div className="container">
 
                         <div className='g3_conteudoCarometro'>
-                            <h1 className="g3_tituloTelas">Carômetro</h1>
-                            <div className="g3_cardsCarometro">
-                                {
+                            <div className='g3_topCarometro'>
+                                <div className='g3_headerCarometro'>
+                                    <h1 className="g3_tituloCarometro  animate__animated animate__fadeInUp">Carômetro</h1>
+                                    <img className="g3_topImgCarometro  animate__animated animate__fadeInUp" src={topCarometro} />
+                                </div>
+                                <div className='g3_navBarCarometro  animate__animated animate__fadeInUp'>
 
-                                    listaFuncionarios.reverse().map((usuario) => {
-                                        //const usuarioSelecionado = () => {listaFuncionarios.find(usuario => usuario.idUsuario == idFuncionarioModal)}
-                                        return (
-                                            <div className="g3_cardFuncionario  animate__animated animate__fadeInUp">
+                                </div>
+                            </div>
+                            <div className='g3_cardsCarometro animate__animated animate__fadeInUp'>
+                                {
+                                    listaFuncionarios.reverse().map((usuario) =>{
+                                        return(
+                                            <div className='g3_cardUsuario'>
                                                 <img className='g3_fotoCarometro' src={'http://localhost:5000/StaticFiles/Images/' + usuario.caminhoFotoPerfil} alt="fotoPerfilCarometro" />
                                                 <span className="g3_spanCarometro">{usuario.nome}</span>
                                                 <span className="g3_spanCarometro">{usuario.idCargoNavigation.nomeCargo}</span>
-                                                {/* <a onClick={openModal} onClickCapture = {() =>setIdFuncionarioModal(usuario.idUsuario)} className='seta_funcionario_carometro'> */}
-                                                {/* <a onClick={handleShow} onClickCapture={() => setIdFuncionarioModal(usuario.idUsuario)}  */}
-                                                {/* <a onClick={() => {handleShow(); }} onClickCapture={() => setFuncionarioModal(usuario)} 
-                                                    
-                                                    className='g3_seta_funcionario_carometro'>
-                                                    <img className='g3_setaCarometro' src={SetaCarometro} alt="setaCard" />
-                                                </a>
-                                                <Modal show={show} onHide={handleClose} centered>
-                                                    
-                                                    <Modal.Header closeButton>
-                                                        <Modal.Title>Informações pessoais</Modal.Title>
-                                                    </Modal.Header>
-                                                    <Modal.Body>
-
-                                                        Nome:<h4>{funcionarioModal.nome}</h4>
-                                                        Email:<h4>{funcionarioModal.email}</h4>    
-                                                        CPF:<h4>{funcionarioModal.cpf}</h4>  
-
-                                                    </Modal.Body>
-                                                    <Modal.Footer>
-                                                        {/* <Button variant="danger" onClick={ExcluirPerfil(funcionarioModal.email)}>
-                                                            Excluir perfil
-                                                        </Button> */}
-                                                        {/* <Button variant="primary" onClick={handleClose}>
-                                                            Save Changes
-                                                        </Button> */}
-                                                    {/* </Modal.Footer> */}
-                                                 {/* </Modal>  */}
-
                                             </div>
+
                                         )
-                                    })
+                                    }
+
+                                    )
                                 }
                             </div>
 
+
+
                         </div>
                     </div>
-                </div>
 
-            </main>
+                </main>
             <Footer />
+            </div>
         </body>
     )
 }
